89fa3e556cbe7af19f4b1dd11fa9701d439eed9e
Carlos M. Martinez <carlos@xt6.us>
2016-05-27 12:36:23 +0200

    fixes to getfile.py

--- cm2c/commons/gen/getfile.py
+++ cm2c/commons/gen/getfile.py
@@ -63,7 +63,7 @@ def getfile(w_url, w_file_name = None, w_update = 3600, ch_size=0):
 
     dp = dprint(log_level)
 
-    dp.log("entering try block\n", 5)
+    dp.log("entering try block\n")
     try:
         dp.log("Getting "+w_url+": ")
         mtime = 0
