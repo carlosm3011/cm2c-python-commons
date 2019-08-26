0527edd870aa1b23bc8131740b2add7013cfcd0c
Carlos M. Martinez <carlos@xt6.us>
2019-08-26 01:51:29 +0000

    commit

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # setup.py
-# cm2c python commns library
+# cm2c python commons library
 # 20170310 v1
+# 20190825 v0.1.8
 
 from setuptools import setup
 
 setup(name='cm2c',
-      version='0.1.5',
+      version='0.1.8',
       description='cm2c python commons',
-      url='https://github.com/carlosm3011/cm2c-python-lib',
+      url='https://github.com/carlosm3011/cm2c-python-commons',
       author='Carlos M. Martinez',
       author_email='carlos@xt6.us',
       license='BSD',
